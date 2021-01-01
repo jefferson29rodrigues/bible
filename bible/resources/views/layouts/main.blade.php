4809048205f56fc73d7932d17d236693998ea454
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-01 13:30:07 -0300

    google fonts bootstrap adicionados

--- bible/resources/views/layouts/main.blade.php
+++ bible/resources/views/layouts/main.blade.php
@@ -6,9 +6,14 @@
 
         <title>@yield('title')</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Fonte do Google -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        
+        <!-- CSS Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+        <!-- CSS da aplicação -->
         <link rel="stylesheet" href="/css/styles.css">
         <script src="/js/scripts.js"></script>
     </head>
