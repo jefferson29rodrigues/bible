d7dd091020382cb826df213916451b4b497cbb2e
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-04 19:27:50 -0300

    resgatando dados dos eventos para a home

--- bible/resources/views/welcome.blade.php
+++ bible/resources/views/welcome.blade.php
@@ -3,17 +3,23 @@
 @section('title', 'Bible')
 
 @section('content')
-<div>
-    <h1>Home</h1>
-    <br><br>
-    <a href="/ranking">ir para o Ranking</a>
-    <br><br>
-    <a href="/chat">ir para o forum</a>
 
-    @if (10 > 14)
-        <p>A condição é verdadeira</p>
-    @else
-        <p>Acondição é falsa</p>
-    @endif
+<div>
+    @foreach($events as $event)
+        <p>{{ $event->title }} -- {{ $event->description }}</p>
+    @endforeach
 </div>
+
+<div>
+    <p>{{ $events[1]->title }}
+</div>
+
+<div>
+    {{ $events }}</p>
+</div>
+
+<div>
+    {{ $event }}</p>
+</div> 
+
 @endsection
--- bible/routes/web.php
+++ bible/routes/web.php
@@ -18,60 +18,6 @@ use App\Http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 
-Route::get('/ranking', function () {
-    $colocacao = 6;
-
-    $primeiro = rand(1,2); 
-    $segundo = rand(0,4); 
-    $terceiro = rand(0,4);  
-    $quarto = rand(0,4); 
-    $quinto = rand(0,4); 
-
-    $pessoas = [
-        'josé' => 1,
-        'joão' => 2,
-        'maria' => 3,
-        'pedro' => 4,
-        'marcos' => 5
-    ];
-
-    if ($pessoas['joão'] == $primeiro) {
-        echo "O primeiro é joão";
-    }
-
-    $arr = [1, 2, 3, 4, 5];
-    $top5 = [$primeiro, $segundo, $terceiro, $quarto, $quinto];
-    
-    return view('ranking', 
-        [
-            'posicao' => $colocacao,
-            'positions' => $arr,
-            'colocados' => $top5,
-            'pessoas' => $pessoas
-        ]
-    );
-});
-
-Route::get('/chat', function () {
-    return view('/forum');
-});
-
-Route::get('/chat/{id}', function ($id) {
-    return view('/chater', ['id' => $id]);
-});
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-    $busca2 = request('idade');
-
-    return view('/products', ['busca' => $busca, 'idade' => $busca2]);
-});
-
-Route::get('/produtos_teste/{id?}', function ($id = null) {
-    return view('/product', ['id' => $id]);
-});
-
 Route::get('/contact', function () {
     return view('/contact');
 });
