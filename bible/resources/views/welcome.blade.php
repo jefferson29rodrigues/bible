ea0d8f8ea35696d3e8fd64cd6ffac43024894f4a
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-05 11:32:32 -0300

    finalizando home da página

--- bible/resources/views/welcome.blade.php
+++ bible/resources/views/welcome.blade.php
@@ -4,6 +4,33 @@
 
 @section('content')
 
+<div id="search-container" class="col-md-12">
+    <h1>Busque um evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p>Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md3">
+            <img src="/img/im34.jpg" alt="{{ $event->title }}">
+            <div class="card-body">
+                <p class="card-date">05/01/21</p>
+                <h5 class="card-title">{{ $event->title }}</h5>
+                <p class="card-participantes">X Participantes</p>
+                <a href="#" class="btn btn-primary">Saber mais</a>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+
+<!-- 
+
 <div>
     @foreach($events as $event)
         <p>{{ $event->title }} -- {{ $event->description }}</p>
@@ -21,5 +48,7 @@
 <div>
     {{ $event }}</p>
 </div> 
+
+-->
 
 @endsection
