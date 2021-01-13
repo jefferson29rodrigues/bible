a020eff7837fa8b88630c34e7fb1e0bc101a7763
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-12 21:23:01 -0300

    busca por filtro terminada

--- bible/resources/views/welcome.blade.php
+++ bible/resources/views/welcome.blade.php
@@ -12,8 +12,13 @@
 </div>
 
 <div id="events-container" class="col-md-12">
+    @if($search)
+    <h2>Buscando por: {{ $search }}</h2>
+    @else
     <h2>Próximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
+
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class= "card col-md-3">
@@ -27,12 +32,16 @@
         </div>
         @endforeach
 
-        @if(count($events) == 0)
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com: {{ $search }} <a href="/"><ion-icon name="play-outline"></ion-icon> Ver lista completa de Eventos!</a></p>
+        @elseif(count($events) == 0)
             <p>Não há eventos disponíveis</p>
         @endif
     </div>
 </div>
 
+{{--
+<!--
 
 <div>
     @foreach($events as $event)
@@ -42,9 +51,11 @@
 
 *************************************
 
+
 <div>
     <p>{{ $events[0]->title }}
 </div>
+
 
 *************************************
 
@@ -58,5 +69,7 @@
     {{ $event }}</p>
 </div> 
 
+-->
+--}}
 
 @endsection
