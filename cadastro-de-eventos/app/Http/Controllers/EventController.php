60998887afe75e2bfc1c5a7595a0f877b0aea146
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-16 01:50:27 -0300

    deletando evento ok

--- cadastro-de-eventos/app/Http/Controllers/EventController.php
+++ cadastro-de-eventos/app/Http/Controllers/EventController.php
@@ -88,4 +88,12 @@ class EventController extends Controller
 
     }
 
+    public function destroy($id) {
+
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento excluido com sucesso!');
+
+    }
+
 }
