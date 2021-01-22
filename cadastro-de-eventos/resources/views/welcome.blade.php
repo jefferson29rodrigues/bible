a2e4e8ba7d657d59e1039f12567507c3bbabf722
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-01-21 23:31:48 -0300

    sistema cadastro de eventos  concluido com sucesso!

--- cadastro-de-eventos/resources/views/welcome.blade.php
+++ cadastro-de-eventos/resources/views/welcome.blade.php
@@ -40,36 +40,4 @@
     </div>
 </div>
 
-{{--
-<!--
-
-<div>
-    @foreach($events as $event)
-        <p>{{ $event->title }} -- {{ $event->description }}</p>
-    @endforeach
-</div>
-
-*************************************
-
-
-<div>
-    <p>{{ $events[0]->title }}
-</div>
-
-
-*************************************
-
-<div>
-    {{ $events }}</p>
-</div>
-
---------------------------------------------------------
-
-<div>
-    {{ $event }}</p>
-</div> 
-
--->
---}}
-
 @endsection
